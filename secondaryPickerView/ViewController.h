340849a47c0106b143e8dcc036c51da847562004
夏勇 <yxia@bianguo.com.cn>
2016-08-05 00:48:23 +0800

    Initial Commit

---
+++ secondaryPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  secondaryPickerView
+//
+//  Created by xiayong on 16/8/5.
+//  Copyright © 2016年 bianguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ secondaryPickerView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  secondaryPickerView
+//
+//  Created by xiayong on 16/8/5.
+//  Copyright © 2016年 bianguo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
